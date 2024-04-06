d0a79f85b07f107e82b74a5c8cb70624bd839dfc
marshall ruan <marshall.ruanel@qq.com>
2024-04-06 23:03:07 +0800

    initialized my emacs configure file

---
+++ init.el
@@ -0,0 +1,18 @@
+;; use the org file to manage emacs config
+(use-package org
+  :ensure t)
+
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-treemacs magit treemacs company-box good-scroll undo-tree mwim mwin rainbow-delimiters dashboard zenburn-theme vertico restart-emacs orderless marginalia embark-consult doom-themes doom-modeline company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
