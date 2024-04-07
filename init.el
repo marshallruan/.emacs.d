cb58b7a80bba3e8e68b913d0e6c4f9f617b5916d
marshall ruan <marshall.ruanel@qq.com>
2024-04-07 18:05:44 +0800

    format the config.org fix-some bugs

--- init.el
+++ init.el
@@ -1,18 +1,9 @@
+; -*- lexical-binding: t -*-
+
 ;; use the org file to manage emacs config
 (use-package org
-  :ensure t)
+  :ensure t
+  :config
+  (setq org-src-preserve-indentation t))
 
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-treemacs magit treemacs company-box good-scroll undo-tree mwim mwin rainbow-delimiters dashboard zenburn-theme vertico restart-emacs orderless marginalia embark-consult doom-themes doom-modeline company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory)
