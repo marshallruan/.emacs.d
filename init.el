7469f1649fcb21592814a0ad5b726f05f875686c
marshall ruan <marshall.ruanel@qq.com>
2024-04-11 22:26:39 +0800

    use evil mode and practice

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ; -*- lexical-binding: t -*-
 
-;; use the org file to manage emacs config
+; make startup faster by reducing the frequency of garbage collection
+(setq gc-cons-threshold 50000000)
+
+; use the org file to manage emacs config
 (use-package org
   :ensure t
   :config
@@ -9,6 +12,9 @@
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 
-;avoid auto custom system modify init.el
+; avoid auto custom system modify init.el
 (setq custom-file (expand-file-name "emacs-custom.el" user-emacs-directory))
 (load custom-file)
+
+; make gc pause faster by decreasing the threshold
+(setq gc-cons-threshold 2000000)
