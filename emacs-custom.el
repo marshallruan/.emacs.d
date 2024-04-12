f2f362ab149595ab34d25ffb4b763c5ba1f1deb7
marshall ruan <marshall.ruanel@qq.com>
2024-04-12 22:45:32 +0800

    add org-agenda function

--- emacs-custom.el
+++ emacs-custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(evil-multiedit evil-matchit iedit evil-snipe evil-nerd-commenter evil-surround evil-collection evil-tutor-sc evil zenburn-theme window-numbering vertico undo-tree treemacs-projectile restart-emacs rainbow-delimiters popper orderless nerd-icons-ivy-rich mwim marginalia magit lsp-treemacs good-scroll embark-consult doom-themes doom-modeline dashboard counsel company-box all-the-icons)))
+   '(org-tempo org-checklist evil-multiedit evil-matchit iedit evil-snipe evil-nerd-commenter evil-surround evil-collection evil-tutor-sc evil zenburn-theme window-numbering vertico undo-tree treemacs-projectile restart-emacs rainbow-delimiters popper orderless nerd-icons-ivy-rich mwim marginalia magit lsp-treemacs good-scroll embark-consult doom-themes doom-modeline dashboard counsel company-box all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
