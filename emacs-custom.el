93c4ddd10ceed75d43cb95bc637809db9358efec
marshallruan <marshall.ruanel@qq.com>
2025-02-09 11:53:23 +0800

    fixed tab with hook on local-mode

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(custom-safe-themes
-   '("7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
+   '("da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" default))
  '(global-display-line-numbers-mode t)
  '(package-selected-packages
    '(solarized-theme simple-httpd ox-publish org-bullets counsel-projectile benchmark-init package-load-metrics package-load-metrcs org-tempo org-checklist evil-multiedit evil-matchit iedit evil-snipe evil-nerd-commenter evil-surround evil-collection evil-tutor-sc evil zenburn-theme window-numbering vertico undo-tree treemacs-projectile restart-emacs rainbow-delimiters popper orderless nerd-icons-ivy-rich mwim marginalia magit lsp-treemacs good-scroll embark-consult doom-themes doom-modeline dashboard counsel company-box all-the-icons))
